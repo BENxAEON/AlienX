2a7fce0463e762d2adc9077f113a13869831262f
BEN <112600905+ALiENxBEN@users.noreply.github.com>
2024-03-27 09:06:52 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -170,7 +170,7 @@ def get_readable_message():
         globals()['STATUS_START'] = STATUS_LIMIT * (PAGES - 1)
         globals()['PAGE_NO'] = PAGES
     for download in list(download_dict.values())[STATUS_START:STATUS_LIMIT+STATUS_START]:
-        
+        msg += f"\n<b>⌬ Elapsed:</b> {get_readable_time(time() - download.message.date.timestamp())} <b>| User:</b> {source(download)}"
         msg += f"<b>{download.status()}...</b>"
         if download.status() not in [MirrorStatus.STATUS_SPLITTING, MirrorStatus.STATUS_SEEDING]:
             msg += f"\n<b>⌬ {get_progress_bar_string(download.progress())} {download.progress()}</b>"
